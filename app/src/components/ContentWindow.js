bf9f3e4e8d221f53219dc91b3205d2c8177e1d5a
Edward Tirado Jr <ed@etdevelopment.net>
2020-10-21 20:22:06 +0100

    Updated css to fix video positioning

--- app/src/components/ContentWindow.js
+++ app/src/components/ContentWindow.js
@@ -4,16 +4,10 @@ import "./ContentWindow.css";
 function ContentWindow(props) {
   return (
     <div className="content-container">
-      <div class="video-display">
-        <iframe
-          title="video-loop"
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/XIhEPwTMjWk?controls=0"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+      <div className="video-display">
+        <video playsinline autoplay="autoplay" muted loop>
+          <source src="https://i.imgur.com/y4oYIiW.mp4" type="video/mp4" />
+	</video>
       </div>
       <div className="sidebar">sidebar</div>
     </div>
