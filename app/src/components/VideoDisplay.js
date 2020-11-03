595d9d7c169d02edd2908bb2d4ea7c0a2ae7fb2b
Edward Tirado Jr <ed@etdevelopment.net>
2020-11-03 10:05:05 -0700

    moved sidebar to left and updated video

--- app/src/components/VideoDisplay.js
+++ app/src/components/VideoDisplay.js
@@ -5,7 +5,7 @@ function VideoDisplay(props) {
   return (
     <div className="video-display">
       <video playsInline autoPlay muted loop>
-        <source src="https://i.imgur.com/y4oYIiW.mp4" type="video/mp4" />
+        <source src="https://i.imgur.com/M4fwZS2.mp4" type="video/mp4" />
       </video>
     </div>
   );
