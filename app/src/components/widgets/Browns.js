324e3dec84725a80dd40848f49cbd82081bcdcdc
Edward Tirado Jr <ed@etdevelopment.net>
2021-02-27 14:11:58 -0700

    added steelers response

--- app/src/components/widgets/Browns.js
+++ app/src/components/widgets/Browns.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./Browns.css";
+import browns_win from "../../assets/browns_logo.jpg";
+import browns_lose from "../../assets/browns_i_wanna_die.jpg";
+import steelers_lose from "../../assets/steelers-lose.gif";
 
 function Browns(props) {
   let background_image;
 
-  if (props.gameResult === "won") {
-    background_image = require("../../assets/browns_logo.jpg");
-  } else {
-    background_image = require("../../assets/browns_i_wanna_die.jpg");
+
+  console.log(props.gameResult);
+  switch (props.gameResult) {
+    case 'won':
+      background_image = browns_win
+      break;
+    case 'beat_steelers':
+      background_image = steelers_lose;
+      break;
+    default:
+      background_image = browns_lose;
+
+
   }
 
   return (
