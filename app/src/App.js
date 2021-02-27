b6fee4f076092f55ef7ae837bd9eba341cb45293
Edward Tirado Jr <ed@etdevelopment.net>
2021-02-27 12:18:45 -0700

    Added debounce function to util.js and updated react-scripts for vulnerabilities

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
-import "./App.css";
 import ContentWindow from "./components/ContentWindow.js";
 import MycroftMessageBus from "./components/MycroftMessageBus.js";
+import "./App.css";
 
 function App() {
   const [display, setDisplay] = useState(null);
 
-  const updateDisplay = debounce((data) => {
+  const updateDisplay = (data) => {
     setDisplay(data);
 
     // reset the display after 15 seconds
-     setTimeout(() => setDisplay(null), 15000);
-  }, 250);
+    setTimeout(() => {
+      setDisplay(null);
+    }, 15000);
+  };
 
   return (
     <div className="App">
@@ -19,22 +21,6 @@ function App() {
       <MycroftMessageBus callback={updateDisplay} />
     </div>
   );
-}
-
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
 }
 
 export default App;
